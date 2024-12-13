4f47ebb1f8f3c93a6e2ab044f19f0ea70cea6191
Raj Shekhar Sinha <raj18110sinha@gmail.com>
2024-12-14 00:54:25 +0530

    bug fix

--- client/blog_frontend/src/api.js
+++ client/blog_frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api/',
-  baseURL: 'https://djangoappbackend.onrender.com',
+  baseURL: 'https://djangoappbackend.onrender.com/api/',
 });
 
 // Interceptor to add access token to the request headers
